6a9fa0201409aedb5d3450ca418cae564b689a84
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-18 18:18:26 +0200

    Closes #4

--- src/main/java/com/maja/sfgdi/SfgDiApplication.java
+++ src/main/java/com/maja/sfgdi/SfgDiApplication.java
@@ -7,7 +7,7 @@ import com.maja.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+
 
 
 @SpringBootApplication
--- src/main/java/com/maja/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/maja/sfgdi/config/GreetingServiceConfig.java
@@ -1,7 +1,4 @@
 package com.maja.sfgdi.config;
-
-import com.maja.pets.CatService;
-import com.maja.pets.DogService;
 import com.maja.pets.PetService;
 import com.maja.pets.PetServiceFactory;
 import com.maja.sfgdi.datasource.FakeDataSource;
@@ -11,7 +8,7 @@ import com.maja.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
